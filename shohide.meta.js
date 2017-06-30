4c920fd67c12d0e606fd98d60ff5108d77705fb8
Sergiy <seranatles@gmail.com>
2017-06-30 11:33:17 +0300

    Create shohide.meta.js

---
+++ shohide.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Bitrix CRM Show/Hide
+// @namespace    bitrix24
+// @version      0.1
+// @description  Bitrix CRM Show/Hide completed check item
+// @author       Lesyuk Serg
+// @include      http://crm.rw-soft.com/*
+// @match        http://crm.rw-soft.com/*/tasks/task/view/*
+// @updateURL    https://raw.githubusercontent.com/LesyukSerg/bitrix24_showhide/master/showhide.meta.js
+// @downloadURL  https://raw.githubusercontent.com/LesyukSerg/bitrix24_showhide/master/showhide.user.js
+// @grant        none
+// ==/UserScript==
