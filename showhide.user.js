54041fd6423b93c0b9a45c6b5a8ebf6ba1ad2915
Sergiy <seranatles@gmail.com>
2017-06-30 11:34:06 +0300

    Update showhide.user.js

--- showhide.user.js
+++ showhide.user.js
@@ -6,8 +6,8 @@
 // @author       Lesyuk Serg
 // @include      http://crm.rw-soft.com/*
 // @match        http://crm.rw-soft.com/*/tasks/task/view/*
-// @updateURL    https://raw.githubusercontent.com/LesyukSerg/bitrix24_showhide/master/showhide.js
-// @downloadURL  https://raw.githubusercontent.com/LesyukSerg/bitrix24_showhide/master/showhide.js
+// @updateURL    https://raw.githubusercontent.com/LesyukSerg/bitrix24_showhide/master/showhide.meta.js
+// @downloadURL  https://raw.githubusercontent.com/LesyukSerg/bitrix24_showhide/master/showhide.user.js
 // @grant        none
 // ==/UserScript==
 
